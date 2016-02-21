585d5dcf08e999d1b919bcb2bb111c98105126fa
Andres <andoch93@gmail.com>
2016-02-21 11:34:40 -0500

    Submitting for hackathon

---
+++ BeautifulScenery/Assets/ledgeScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class ledgeScript : MonoBehaviour {
+
+    private bool check;
+    private AudioSource ads;
+
+    // Use this for initialization
+    void Start () {
+        check = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!check && !onledge())
+        {
+            Debug.Log("yeeeeeee\n");
+            ads.enabled = !ads.enabled;
+            check = true;
+        }
+
+    }
+
+    bool onledge()
+    {
+        if (GameObject.Find("FPSController").transform.position.z < GameObject.Find("Cube").transform.position.z - 2 - GameObject.Find("Cube").transform.localScale.z / 2)
+        {
+            Debug.Log("number: " + (GameObject.Find("Cube").transform.position.z - 2 - GameObject.Find("Cube").transform.localScale.z / 2));
+            return false;
+        }
+        return true;
+    }
+}
